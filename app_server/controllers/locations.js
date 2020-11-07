89b235c5914fb9cefb4b38764f90a03471187055
Mustafa I <milikkan@yahoo.com>
2020-11-08 02:25:14 +0300

    Move daa back from views to controllers

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,16 +1,101 @@
 /* GET homepage */
 const homeList = (req, res) => {
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', { 
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    locations: [
+      {
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+      },
+      {
+        name: 'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '200m'
+      },
+      {
+        name: 'Burger Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 2,
+        facilities: ['Food', 'Premium wifi'],
+        distance: '250m'
+      }
+    ]
+  });
 };
 
 /* GET location info page */
 const locationInfo = (req, res) => {
-  res.render('location-info', { title: 'Location info' });
+  res.render('location-info', { 
+    title: 'Starcups',
+    pageHeader: {
+      title: 'Starcups'
+    },
+    sidebar: {
+      context: "is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.",
+      callToAction: "If you've been and you like it - or if you don't - please leave a review to help other people just like you."
+    },
+    location: {
+      name: "Starcups",
+      address: "125 High Street, Reading, RG6 1PS",
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      coords: {
+        lat: 10.000,
+        lng: 11.000
+      },
+      openingTimes: [
+        {
+          days: 'Monday - Friday',
+          opening: '7:00am',
+          closing: '7:00pm',
+          closed: false
+        },
+        {
+          days: 'Saturday',
+          opening: '8:00am',
+          closing: '5:00pm',
+          closed: false
+        },
+        {
+          days: 'Sunday',
+          closed: true
+        }
+      ],
+      reviews: [
+        {
+          author: 'Simon Holmes',
+          rating: 5,
+          timestamp: '16 July 2013',
+          reviewText: "What a great place. \nI can't say enough good things about it."
+        },
+        {
+          author: 'Charlie Chaplin',
+          rating: 3,
+          timestamp: '16 July 2013',
+          reviewText: "It was okay. Coffee wasn't great but wifi was fast."
+        }
+      ]
+    }
+  });
 };
 
 /* GET add review page */
 const addReview = (req, res) => {
-  res.render('location-review-form', { title: 'Add review' });
+  res.render('location-review-form', { 
+    title: 'Review Starcups on Loc8r',
+    pageHeader: {
+      title: 'Review Starcups'
+    } 
+  });
 };
 
 module.exports = {
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,6 +1,9 @@
 /* GET 'about' page */
 const about = (req, res) => {
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', { 
+    title: 'About Loc8r',
+    content: "Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan." 
+  });
 };
 
 module.exports = {
